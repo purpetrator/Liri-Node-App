06693426fe4b5da114aeebebceeb5c5440ebecf2
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-07 21:19:12 -0500

    Set up initial file structure and created OMDB call

---
+++ liri.js
@@ -0,0 +1,89 @@
+//Add code to read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+// var keys = require("./keys.js");
+
+//You should then be able to access your keys information like so:
+// var spotify = new Spotify(keys.spotify);
+
+var action = process.argv[2];
+
+// Here i'm writing a switch statement that will call certain functions
+switch (action) {
+  case "concert-this":
+    concertThis();
+    break;
+
+  case "spotify-this-song":
+    spotifyThis();
+    break;
+
+  case "movie-this":
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    doWhat();
+    break;
+
+  default:
+    return;
+}
+
+function concertThis() {}
+
+function spotifyThis() {}
+
+function movieThis() {
+  var axios = require("axios");
+  var nodeArgs = process.argv;
+  var movieName = "";
+  //   var movieName = process.argv[3];
+
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      movieName = movieName + "+" + nodeArgs[i];
+    } else {
+      movieName += nodeArgs[i];
+    }
+  }
+
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      console.log("Title: " + response.data.Title);
+      console.log("Release Year: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+      console.log("Produced In: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
+
+function doWhat() {}
